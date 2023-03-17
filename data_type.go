94da15a5c3d4b6e51622f2b62d94c11311f7630b
fhs-mac <fenghshia@gmail.com>
2023-03-17 15:23:10 +0800

    +

---
+++ data_type.go
@@ -0,0 +1,38 @@
+package main
+
+func data_type() {
+	// 布尔型
+	var tb1 bool = true
+	println(tb1)
+	var tb2 bool = false
+	println(tb2)
+	// 数字类型
+	// int
+	var tint1 uint8 = 8
+	println(tint1)
+	var tint2 uint16 = 16
+	println(tint2)
+	var tint3 uint32 = 32
+	println(tint3)
+	var tint4 uint64 = 64
+	println(tint4)
+	var tint5 int8 = -8
+	println(tint5)
+	var tint6 int16 = -16
+	println(tint6)
+	var tint7 int32 = -32
+	println(tint7)
+	var tint8 int64 = -64
+	println(tint8)
+	// float
+	var tf1 float32 = 3.2
+	println(tf1)
+	var tf2 float64 = 6.4
+	println(tf2)
+	// 字符串类型
+	// 派生类型
+	/*
+	var t1 int = 1 equa t1 := 1
+	const t2 float = 3.2 can not be change in programe
+	*/
+}
--- hello.go
+++ hello.go
@@ -5,3 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Golang !")
 }
+
+// 单行注释
+/*
+多行注释
+*/
